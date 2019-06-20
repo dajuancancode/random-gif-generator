e540eba6befb2cd85e9f64d621dbd6fe6c22c5ee
DaJuan Harris <dajuancancode@gmail.com>
2019-06-20 18:27:38 -0400

    Add 404 and Help routes

--- src/server.js
+++ src/server.js
@@ -1,22 +1,29 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const gifRequest = require('./utils/gifRequest');
 
 // Setup paths for views
 const viewPath = path.join(__dirname, '../templates/views');
 const staticPath = path.join(__dirname, '../public');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.set('view engine', 'hbs');
 app.set('views', viewPath);
+hbs.registerPartials(partialsPath);
 
 app.use(express.static(staticPath));
 
 app.get('', (req, res) => {
-  res.render('index');
+  res.render('index', { title: 'Random Gif Generator' });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', { title: 'Help' });
 });
 
 app.get('/gif', (req, res) => {
@@ -29,6 +36,10 @@ app.get('/gif', (req, res) => {
     }
     res.send({ gifUrl: data });
   });
+});
+
+app.get('*', (req, res) => {
+  res.render('404', { title: '404' });
 });
 
 app.listen(port, () => {
